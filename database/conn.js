444be4f133a4976d69c8aee65f11d04ed2cf29ef
SAIKAT SAMANTA <saikat.samanta@hy-vee.com>
2025-09-17 21:05:01 +0530

    message

--- database/conn.js
+++ database/conn.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.connect(`mongodb+srv://sparkyaipvtltd_db_user:${process.env.MONGODB_PASS}@cluster0.f19xawc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
 .then(()=>{
-    console.log("Connected to DB ")
+    console.log("Connected to DB ✌️")
 })
 .catch((err)=>{
     console.log("Disconnected from DB", err)
