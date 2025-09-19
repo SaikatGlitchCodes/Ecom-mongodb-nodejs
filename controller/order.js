bbb09c7e01f9543501dcc5a81f1e6e9d4c0c8f11
RonitJangam <ronitjangamcr7@gmail.com>
2025-09-19 20:08:00 +0530

    Orders

---
+++ controller/order.js
@@ -0,0 +1,33 @@
+const express=require("express");
+const router=express.Router();
+const app = express();
+
+const Ordermodel=require('../model/orders_model');
+
+
+
+console.log("Orders page")
+router.get('/orders',async (req,res)=>{
+    try{
+        const response=await Ordermodel.find()
+        res.status(200).json(response)
+    }
+    catch(err){
+        res.status(401).json({error:err})
+    }
+})
+
+router.post('/orders',async (req,res)=>{
+    try{
+        const Order_prod=new Ordermodel(req.body)
+        const response= await Order_prod.save()
+        res.status(200).json(response);
+    }
+    catch(err){
+        res.status(401).json({error:err})
+    }
+})
+
+module.exports=router;
+
+
